0df696bbda43c6109aa2ab704072b846123c4fdd
huynguyenj <nguyenhuyjobs@gmail.com>
2026-09-13 22:31:13 +0700

    feat: update navigate depend on role in useLogin hook

--- src/features/authentication/hooks/useLogin.tsx
+++ src/features/authentication/hooks/useLogin.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 // import type { UserType } from '../types/user'
 import { toast } from 'react-toastify'
 import useApiCall from '@/hooks/useApiCall'
+import type { LoginType } from '../types/user'
+import { useNavigate } from 'react-router'
 
 const LoginFormSchema = z.object({
   email: z.email({ error: 'Hãy nhập đúng định dạng của email @gmail.com' }),
@@ -14,8 +16,8 @@ const LoginFormSchema = z.object({
 export type LoginFormType = z.infer<typeof LoginFormSchema>
 export default function useLogin() {
   const { register, handleSubmit, formState: { errors } } = useForm<LoginFormType>({ resolver: zodResolver(LoginFormSchema) })
-  const { execute, loading } = useApiCall()
-  // const navigate = useNavigate()
+  const { execute, loading } = useApiCall<LoginType>()
+  const navigate = useNavigate()
   const onSubmit = async (loginFormData: LoginFormType) => {
       const data = await execute({
         apiUrl: '/Auth/login',
@@ -23,8 +25,9 @@ export default function useLogin() {
         type: 'public',
         body: loginFormData
       })
-      console.log(data);
       toast.success('Đăng nhập thành công')
+      if (data.data.user.role == 'Admin') navigate('/admin')
+      if (data.data.user.role == 'Lecturer') navigate('/lecture')
       
   }
   return { onSubmit, register, handleSubmit, errors, loading }
